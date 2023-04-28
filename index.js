f8d92c28b721d4bf8368b324fec55f07ec62eab8
Prakriti Pokharel <iprakritip@gmail.com>
2023-04-28 14:30:31 +0545

    add clear functionality

--- index.js
+++ index.js
@@ -1,20 +1,61 @@
 const canvas = document.querySelector("#draw");
+const small = document.querySelector(".small");
+const medium = document.querySelector(".medium");
+const large = document.querySelector(".large");
+// const clear = document.querySelector(".clear")
+
 const context = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+
+innerHeight = window.innerHeight / 1.3;
+innerWidth = window.innerWidth;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 context.strokeStyle = "#BADA55";
 context.lineJoin = "round";
 context.lineCap = "round";
-context.lineWidth = 50;
+// context.lineWidth = 50;
 context.globalCompositeOperation = "multiply";
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
-let direction = true;
+// let direction = true;
+
+let isSmall = false;
+let isMedium = true;
+let isLarge = false;
+
+small.addEventListener("click", () => {
+    isSmall = true;
+    isMedium = false;
+    isLarge = false;
+    console.log(isSmall);
+})
+medium.addEventListener("click", () => {
+    isMedium = true;
+    isLarge = false;
+    isSmall = false;
+})
+large.addEventListener("click", () => {
+    isLarge = true;
+    isMedium = false;
+    isSmall = false;
+})
+
+function handleLineWidth() {
+    if (isSmall) {
+        context.lineWidth = 20;
+        console.log("small");
+    } else if (isMedium) {
+        context.lineWidth = 40;
+    } else {
+        context.lineWidth = 80;
+    }
+}
 
 function draw(e) {
+    handleLineWidth();
     if (!isDrawing) return;//to stop the function from running if mouse isnt down
     context.strokeStyle = `hsl(${hue}, 80%, 50%)`;
     context.beginPath();
@@ -28,16 +69,14 @@ function draw(e) {
     if (hue >= 360) {
         hue = 0;
     }
-    if (context.lineWidth >= 100 || context.lineWidth <= 1) {
-        direction = !direction;
-    }
-    if (direction) {
-        context.lineWidth++;
-    }else{
-        context.lineWidth--;
-    }
-    // context.lineWidth++;
-
+    // if (context.lineWidth >= 100 || context.lineWidth <= 1) {
+    //     direction = !direction;
+    // }
+    // if (direction) {
+    //     context.lineWidth++;
+    // }else{
+    //     context.lineWidth--;
+    // }
 }
 canvas.addEventListener("mousedown", (e) => {
     isDrawing = true;
